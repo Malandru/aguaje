1954d0eed11b15942b0fcfc7950fafa410027033
Andres Martinez <andres.martinez@expertos-ti.com>
2021-03-30 17:30:28 -0600

    Libreria de funciones en comun

---
+++ common/include/logging.hpp
@@ -0,0 +1,8 @@
+#ifndef LOGGING_HPP
+#define LOGGING_HPP
+
+#include <string>
+
+void log(std::string);
+
+#endif
---
+++ common/logging.cpp
@@ -0,0 +1,8 @@
+#include <logging.hpp>
+#include <iostream>
+
+void log(std::string message)
+{
+    std::cout << "Logging message..." << std::endl;
+    std::cout << message << std::endl;
+}
